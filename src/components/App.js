7377ba00d8b9b8a10d01eb180aa1180e343865e8
Hans Thummel <enikath@gmail.com>
2018-12-04 16:11:50 -0600

    BLACK DIAMOND ACTIVATED!

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import './App.css';
+import "./App.css";
 
-import Header from './Header/Header';
-import Compose from './Compose/Compose';
+import Header from "./Header/Header";
+import Compose from "./Compose/Compose";
+import Post from "./Post/Post";
 
 class App extends Component {
   constructor() {
@@ -13,38 +15,102 @@ class App extends Component {
       posts: []
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.baseurl = "https://practiceapi.devmountain.com/api";
+
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
+
+    this.searchPost = this.searchPost.bind(this);
   }
-  
+
   componentDidMount() {
-
+    axios
+      .get(`https://practiceapi.devmountain.com/api/posts`)
+      .then(response => {
+        this.setState({ posts: response.data });
+        console.log("componentDidMount() GET: " + response);
+      });
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    console.log(text);
+    axios
+      .put(`${this.baseurl}/posts?id=${id}`, { text })
+      .then(resp => {
+        console.log(resp);
+        this.setState({ posts: resp.data });
+        // console.log("updatePost() put: " + resp);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios
+      .delete(`${this.baseurl}/posts?id=${id}`)
+      .then(resp => {
+        console.log(resp);
+        this.setState({ posts: resp.data });
+        // console.log("updatePost() put: " + resp);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
-  createPost() {
+  createPost(text) {
+    axios
+      .post(`${this.baseurl}/posts`, { text })
+      .then(resp => {
+        console.log(resp);
+        this.setState({ posts: resp.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
+  searchPost(text) {
+    var sPosts = [];
+
+    axios
+      .get(`${this.baseurl}/posts`)
+      .then(resp => {
+        resp.data.map(elm => {
+          if (elm.text.includes(text)) {
+            sPosts.push(elm);
+          }
+        });
+        //console.log(sPosts);
+        this.setState({ posts: sPosts });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   render() {
     const { posts } = this.state;
+    console.log(posts);
 
     return (
       <div className="App__parent">
-        <Header />
+        <Header searchPostFn={this.searchPost} />
 
         <section className="App__content">
-
-          <Compose />
-          
+          <Compose createPostFn={this.createPost} />
+          {posts.map(elm => (
+            <Post
+              key={elm.id}
+              text={elm.text}
+              date={elm.date}
+              updatePostFn={this.updatePost}
+              deletePostFn={this.deletePost}
+              id={elm.id}
+            />
+          ))}
         </section>
       </div>
     );
--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -1,23 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './Search.css';
+import "./Search.css";
 
-import SearchIcon from 'react-icons/lib/md/search';
+import SearchIcon from "react-icons/lib/md/search";
 
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *HEADER* COMPONENT
 
 export default class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      search: "",
+      fText: ""
+    };
+  }
   render() {
     return (
       <section className="Search__parent">
-
         <div className="Search__content">
-          <input placeholder="Search Your Feed" />
+          <input
+            placeholder="Search Your Feed"
+            text={this.state.fText}
+            onChange={e => {
+              this.setState({ fText: e.target.value });
+            }}
+          />
 
-          <SearchIcon id="Search__icon" />
+          <SearchIcon
+            id="Search__icon"
+            onClick={() => {
+              this.props.searchPostFn(this.state.fText);
+              this.setState({ fText: "" });
+            }}
+          />
         </div>
-        
       </section>
-    )
+    );
   }
 }
